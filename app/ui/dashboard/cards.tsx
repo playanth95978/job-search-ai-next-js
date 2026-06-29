6fec82b8343553e29c313052bf9fd8784f03d670
aattia <anthony.attia@redstone.nc>
2026-06-30 08:24:57 +1100

    📝 **feat**(app/dashboard/(overview)/page.tsx): remplace les cartes statiques par un composant dynamique CardWrapper avec état de chargement

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,6 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+import { BanknotesIcon, ClockIcon, InboxIcon, UserGroupIcon, } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/font';
+import { fetchCardData } from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,20 +10,24 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  return (
-    <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
-    </>
-  );
+    const {
+        totalPaidInvoices,
+        totalPendingInvoices,
+        numberOfInvoices,
+        numberOfCustomers,
+    } = await fetchCardData();
+    return (
+        <>
+            <Card title="Collected" value={ totalPaidInvoices } type="collected"/>
+            <Card title="Pending" value={ totalPendingInvoices } type="pending"/>
+            <Card title="Total Invoices" value={ numberOfInvoices } type="invoices"/>
+            <Card
+                title="Total Customers"
+                value={ numberOfCustomers }
+                type="customers"
+            />
+        </>
+    );
 }
 
 export function Card({
